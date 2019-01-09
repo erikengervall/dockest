1b30c30a940f66d0f67ccb7e15fc8b0fdfa5d14f
Erik Engervall <erik.engervall@klarna.com>
2019-01-09 22:13:13 +0100

    dockestrc

---
+++ example/.dockestrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  jest: {
+    lib: require('jest'),
+    silent: false, // true
+    verbose: false, // false
+    forceExit: true, // false
+    watchAll: false, // ?
+    projects: ['.'], // null
+  },
+  postgres: [
+    {
+      seeder: '20181130152743-demo-user',
+      connectionTimeout: 15,
+      responsivenessTimeout: 15,
+      label: 'dockest.project=postgres1',
+      username: 'erik',
+      password: 'secretpw',
+      db: 'dab',
+      host: 'localhost',
+      port: 5434,
+      service: 'postgres1',
+    },
+  ],
+  dockest: {
+    verbose: true,
+    exitHandler: error => {
+      console.log('error', error)
+    },
+    dockerComposeFile: 'docker-compose-test.yml',
+  },
+}
