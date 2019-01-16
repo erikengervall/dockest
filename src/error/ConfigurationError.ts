c1111c0f2ba3c5f7cd80d8a2579ee9efb0b4024f
Erik Engervall <erik.engervall@klarna.com>
2019-01-16 16:05:53 +0100

    maintain error stack trace for configError

--- src/error/ConfigurationError.ts
+++ src/error/ConfigurationError.ts
@@ -4,6 +4,7 @@ export class ConfigurationError extends Error {
   constructor(message: string) {
     super(`${ICONS.ERROR} Invalid configuration: ${message}}`)
 
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, ConfigurationError)
     }
