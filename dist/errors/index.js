ef2cc86c94015438015f725846fa336e88436909
Erik Engervall <erik.engervall@klarna.com>
2019-01-21 23:28:30 +0100

    go back to including eveerything in build

---
+++ dist/errors/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ConfigurationError_1 = require("./ConfigurationError");
+exports.ConfigurationError = ConfigurationError_1.default;
+var DockestError_1 = require("./DockestError");
+exports.DockestError = DockestError_1.default;
+//# sourceMappingURL=index.js.map
