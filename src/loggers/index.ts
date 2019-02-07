2a99728b41a652a751ea2055d011ad0214281792
Erik Engervall <erik.engervall@klarna.com>
2019-02-07 22:18:57 +0100

    is it truly the loggers that are acting up?

--- src/loggers/index.ts
+++ src/loggers/index.ts
@@ -1,4 +1,44 @@
-export { default as GlobalLogger } from './GlobalLogger'
-export { default as RunnerLogger } from './RunnerLogger'
-export { default as RunnerUtilsLogger } from './RunnerUtilsLogger'
-export { default as JestLogger } from './JestLogger'
+const global: any = {
+  info: () => ({}),
+  loading: () => ({}),
+  error: () => ({}),
+}
+const runner: any = {
+  setup: () => ({}),
+  setupSuccess: () => ({}),
+  startContainer: () => ({}),
+  startContainerSuccess: () => ({}),
+  checkHealth: () => ({}),
+  checkHealthSuccess: () => ({}),
+  checkResponsiveness: () => ({}),
+  checkResponsivenessSuccess: () => ({}),
+  checkConnection: () => ({}),
+  checkConnectionSuccess: () => ({}),
+  teardown: () => ({}),
+  teardownSuccess: () => ({}),
+  stopContainer: () => ({}),
+  stopContainerSuccess: () => ({}),
+  removeContainer: () => ({}),
+  removeContainerSuccess: () => ({}),
+  shellCmd: () => ({}),
+}
+
+const runnerutil: any = {
+  customShellCmd: () => ({}),
+  customShellCmdSuccess: () => ({}),
+}
+const jest: any = {
+  success: () => ({}),
+  failed: () => ({}),
+  error: () => ({}),
+}
+
+export { global as GlobalLogger }
+export { runner as RunnerLogger }
+export { runnerutil as RunnerUtilsLogger }
+export { jest as JestLogger }
+
+// export { default as GlobalLogger } from './GlobalLogger'
+// export { default as RunnerLogger } from './RunnerLogger'
+// export { default as RunnerUtilsLogger } from './RunnerUtilsLogger'
+// export { default as JestLogger } from './JestLogger'
