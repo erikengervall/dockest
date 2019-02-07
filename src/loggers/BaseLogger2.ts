b4f12cee53abdad3bdc9714dcdaf22c1acdaeb94
Erik Engervall <erik.engervall@klarna.com>
2019-02-07 22:44:19 +0100

    ...

--- src/loggers/BaseLogger2.ts
+++
@@ -1,9 +0,0 @@
-class BaseLogger {
-  private static baseLoggerInstance: BaseLogger
-
-  constructor() {
-    return BaseLogger.baseLoggerInstance || (BaseLogger.baseLoggerInstance = this)
-  }
-}
-
-export default BaseLogger
