a510b54ef81238f2077bcdbdcce030ef31824aa7
Travis CI <erik.engervall@gmail.com>
2019-02-12 17:08:37 +0100

    -

--- .travis-commit-dist.sh
+++ .travis-commit-dist.sh
@@ -7,8 +7,7 @@ setup_git() {
 
 commit_build_files() {
   yarn build
-  git fetch --all
-  git checkout $BRANCH
+  git checkout -b $BRANCH
   git add ./dist
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
