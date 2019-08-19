27f0f2bd7c5c2101b52f615d7f571973ce3f4c02
Erik Engervall <erik.engervall@klarna.com>
2019-08-19 21:51:32 +0200

    introducing the simple runner 🎉

---
+++ src/runners/SimpleRunner/index.spec.ts
@@ -0,0 +1,18 @@
+import SimpleRunner from './index'
+
+const SimpleRunner1 = new SimpleRunner({ service: 'r1' })
+const SimpleRunner2 = new SimpleRunner({ service: 'r2' })
+
+describe('SimpleRunner', () => {
+  it('should create unique instances', () => {
+    expect(SimpleRunner1).not.toBe(SimpleRunner2)
+  })
+
+  it('should fail validation', () => {
+    expect(
+      () =>
+        // @ts-ignore
+        new SimpleRunner({}),
+    ).toThrow(/service: Schema-key missing in config/)
+  })
+})
