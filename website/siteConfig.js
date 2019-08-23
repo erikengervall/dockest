e1822cd663ecfe0a76e75309a4090973153e11db
Erik Engervall <erik.engervall@gmail.com>
2019-08-23 16:39:05 +0200

    Added search to website docs!

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,8 +8,6 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-const engineeringBlogPostUrl = 'https://engineering.klarna.com/node-js-integration-testing-with-ease-fab5f8d29163'
-
 const siteConfig = {
   title: 'Dockest', // Title for your website.
   tagline:
@@ -17,7 +15,7 @@ const siteConfig = {
   url: 'https://erikengervall.github.io', // Your website URL
   baseUrl: '/dockest/', // Base URL for your project
   organizationName: 'erikengervall',
-  projectName: 'Dockest', // Used for publishing and more
+  projectName: 'dockest', // Used for publishing and more
 
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -58,6 +56,13 @@ const siteConfig = {
     ]
   },
   */
+
+  algolia: {
+    apiKey: '9bf9f3d6b00b633293ccea7bbd73a7d1',
+    indexName: 'dockest',
+    algoliaOptions: {}, // Optional, if provided by Algolia
+  },
+
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} Erik Engervall`,
 
@@ -91,13 +96,7 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   repoUrl: 'https://github.com/erikengervall/dockest',
-  engineeringBlogPostUrl,
-
-  algolia: {
-    apiKey: '9bf9f3d6b00b633293ccea7bbd73a7d1',
-    indexName: 'dockest',
-    algoliaOptions: {}, // Optional, if provided by Algolia
-  },
+  engineeringBlogPostUrl: 'https://engineering.klarna.com/node-js-integration-testing-with-ease-fab5f8d29163',
 }
 
 module.exports = siteConfig
