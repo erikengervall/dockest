d1e0cd9897510b718b423e5ef6f9b5c1e4a5a36f
Erik Engervall <erik.engervall@gmail.com>
2019-09-05 13:49:52 +0200

    Corrected serviceName for Users

--- examples/node-to-node/users/constants.ts
+++ examples/node-to-node/users/constants.ts
@@ -3,5 +3,5 @@ import { default as pathLib } from 'path'
 export const EXTERNAL_PORT = 1337
 export const INTERNAL_PORT = EXTERNAL_PORT
 export const BASE_URL = `http://localhost:${EXTERNAL_PORT}/`
-export const SERVICE_NAME = 'a_microservice'
+export const SERVICE_NAME = 'users'
 export const PATH = pathLib.resolve(__dirname)
