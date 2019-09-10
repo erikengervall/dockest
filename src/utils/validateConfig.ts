2fcbb81d455bbd0eb23a14a704b0cf7f6fc18ef4
Erik Engervall <erik.engervall@gmail.com>
2019-09-10 16:36:09 +0200

    Refactor and require at least one of Image/Build opts

--- src/utils/validateConfig.ts
+++ src/utils/validateConfig.ts
@@ -5,6 +5,11 @@ import ConfigurationError from '../errors/ConfigurationError'
 export default (schema: { [key: string]: any }, config: RunnerConfig) => {
   const failures = validateTypes(schema, config)
 
+  const { build, image } = config
+  if (!image && !build) {
+    failures.push(`Missing both "image" and "build", at least one has to be present.`)
+  }
+
   if (failures.length > 0) {
     throw new ConfigurationError(`${failures.join('\n')}`)
   }
