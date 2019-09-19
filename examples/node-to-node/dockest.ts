b6f748f76e79c169531f566239be2260bc570be1
Erik Engervall <erik.engervall@gmail.com>
2019-09-19 23:13:38 +0200

    Adjust node-to-node example to new interface

--- examples/node-to-node/dockest.ts
+++ examples/node-to-node/dockest.ts
@@ -3,28 +3,26 @@ import { SERVICE_NAME as ORDERS_SERVICE_NAME } from './orders/constants'
 import Dockest, { logLevel, runners } from '../../src'
 
 const dockest = new Dockest({
-  jest: {
-    lib: require('jest'),
-  },
   opts: {
-    logLevel: logLevel.DEBUG,
-    dumpErrors: true,
     afterSetupSleep: 1,
+    dumpErrors: true,
+    logLevel: logLevel.DEBUG,
   },
-  runners: [
-    new runners.GeneralPurposeRunner({
-      service: USERS_SERVICE_NAME,
-      ports: { '1337': '1337' },
-      build: './users',
-      networks: ['bueno'],
-    }),
-    new runners.GeneralPurposeRunner({
-      service: ORDERS_SERVICE_NAME,
-      ports: { '1338': '1338' },
-      build: './orders',
-      networks: ['bueno'],
-    }),
-  ],
 })
+
+dockest.attachRunners([
+  new runners.GeneralPurposeRunner({
+    service: USERS_SERVICE_NAME,
+    ports: { '1337': '1337' },
+    build: './users',
+    networks: ['bueno'],
+  }),
+  new runners.GeneralPurposeRunner({
+    service: ORDERS_SERVICE_NAME,
+    ports: { '1338': '1338' },
+    build: './orders',
+    networks: ['bueno'],
+  }),
+])
 
 dockest.run()
