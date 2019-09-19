c8174bcdcae410a1509a453f9480b8af180233c7
Erik Engervall <erik.engervall@gmail.com>
2019-09-19 23:21:05 +0200

    Update tests for transformComposeObjToRunners

--- src/onInstantiation/generateComposeFile/transformComposeObjToRunners.spec.ts
+++ src/onInstantiation/generateComposeFile/transformComposeObjToRunners.spec.ts
@@ -1,22 +1,11 @@
 import transformComposeObjToRunners from './transformComposeObjToRunners'
 import testUtils from '../../testUtils'
-import { singlePostgres, postgresAndRedis } from '../../../fixtures/composeObj'
+import { postgresAndRedis } from '../../../fixtures/composeObj'
 
 const { createDockestConfig, initializedRunners } = testUtils({ realLoggers: true })
 
 describe('transformComposeObjToRunners', () => {
-  it('should detect conflict and use the already existing runner', () => {
-    const dockestConfig = createDockestConfig({
-      runners: Object.values(initializedRunners),
-      realLoggers: true,
-    })
-
-    const result = transformComposeObjToRunners(dockestConfig, singlePostgres)
-
-    expect(result).toMatchSnapshot()
-  })
-
-  it('should detect conflict and use the already existing runner', () => {
+  it('should detect conflict and prioritize attachedRunners', () => {
     const dockestConfig = createDockestConfig({
       runners: [initializedRunners.redisRunner],
       realLoggers: true,
