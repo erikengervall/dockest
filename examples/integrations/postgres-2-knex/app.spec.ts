f23fe167b511cfc0098b9ff65b5f4e4658bdfbd8
Erik Engervall <erik.engervall@gmail.com>
2019-09-20 17:22:13 +0200

    remove old unit test toggles

--- examples/integrations/postgres-2-knex/app.spec.ts
+++ examples/integrations/postgres-2-knex/app.spec.ts
@@ -1,18 +1,11 @@
-import dotenv from 'dotenv'
 import app from './app'
-import { runOrSkip } from '../testUtils'
 
 const { seedBanana } = require('./data.json') // eslint-disable-line @typescript-eslint/no-var-requires
 
-const env: any = dotenv.config().parsed
+describe('postgres-2-knex', () => {
+  it('should get first entry', async () => {
+    const { firstEntry } = await app()
 
-const specWrapper = () =>
-  describe('postgres-2-knex', () => {
-    it('should get first entry', async () => {
-      const { firstEntry } = await app()
-
-      expect(firstEntry).toEqual(expect.objectContaining(seedBanana))
-    })
+    expect(firstEntry).toEqual(expect.objectContaining(seedBanana))
   })
-
-runOrSkip(env.postgres2knex_enabled, specWrapper)
+})
