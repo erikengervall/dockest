1036747c12c3e7cd8310cc06938eac3d190b2667
n1ru4l <laurinquast@googlemail.com>
2019-09-26 10:16:29 +0200

    chore: disable tests outside container

--- examples/aws-code-build-example/run_tests.sh
+++ examples/aws-code-build-example/run_tests.sh
@@ -7,8 +7,8 @@ yarn pack --filename examples/aws-code-build-example/dockest.tgz
 cd examples/aws-code-build-example
 
 # build dockest
-yarn install
-yarn test
+# yarn install
+# yarn test
 
 # build with dockest inside docker container
 ./codebuild_build.sh -i n1ru4l/aws-codebuild-node -a .artifacts
