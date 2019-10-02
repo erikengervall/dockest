9919a8feb4e296c1e372cdd5f47bcf13562fde1d
n1ru4l <laurinquast@googlemail.com>
2019-10-02 08:04:56 +0200

    test: pin docker image to hash

--- examples/aws-code-build-example/run_tests.sh
+++ examples/aws-code-build-example/run_tests.sh
@@ -13,4 +13,4 @@ yarn test
 
 # build with dockest inside docker container
 rm -rf node_modules
-./codebuild_build.sh -i n1ru4l/aws-codebuild-node -a .artifacts
+./codebuild_build.sh -i n1ru4l/aws-codebuild-node:7712cfae8d65fd3b704f74e84f688739de5bd357 -a .artifacts
