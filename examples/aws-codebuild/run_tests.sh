d4a4c914823c7120795b5a9449b6b246a6793574
Erik Engervall <erik.engervall@gmail.com>
2019-10-04 10:37:58 +0200

    Replace `rm -rf` with rimraf

--- examples/aws-codebuild/run_tests.sh
+++ examples/aws-codebuild/run_tests.sh
@@ -12,5 +12,5 @@ yarn install --no-lockfile
 #yarn test
 
 # build with dockest inside docker container
-rm -rf node_modules
+yarn clean:node_modules
 ./codebuild_build.sh -i n1ru4l/aws-codebuild-node:7712cfae8d65fd3b704f74e84f688739de5bd357 -a .artifacts
