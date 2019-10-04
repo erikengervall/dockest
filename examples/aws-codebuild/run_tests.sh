85cb1d982a2af07db2c03e13ec6f9f5a8bdbe532
Erik Engervall <erik.engervall@gmail.com>
2019-10-04 10:58:52 +0200

    Remove sneaky yarn.lock Revert rimraf for aws-codebuild

--- examples/aws-codebuild/run_tests.sh
+++ examples/aws-codebuild/run_tests.sh
@@ -9,8 +9,8 @@ cd examples/aws-codebuild
 # build dockest
 yarn cache clean
 yarn install --no-lockfile
-#yarn test
+yarn test
 
 # build with dockest inside docker container
-yarn clean:node_modules
+rm -rf node_modules
 ./codebuild_build.sh -i n1ru4l/aws-codebuild-node:7712cfae8d65fd3b704f74e84f688739de5bd357 -a .artifacts
