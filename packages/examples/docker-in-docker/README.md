e9cc6014bff236b9412f658ebb14530fc3ee7e78
n1ru4l <laurinquast@googlemail.com>
2020-03-30 11:22:03 +0200

    feat: use official docker in docker image

---
+++ packages/examples/docker-in-docker/README.md
@@ -0,0 +1,16 @@
+# Docker in Docker Example
+
+## Motivation
+
+This example will run a docker daemon inside a docker container and run jest against it.
+
+Should you do this? Probably not. You can read more here: https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
+
+Some environments (such as AWS Codebuild) will however require using this method, as they do not provide a way of injecting the host docker socket into the build container.
+
+## How can I use this example?
+
+```bash
+./prepare.sh
+./run.sh
+```
--- packages/examples/docker-in-docker/src/run-test.sh
+++ packages/examples/docker-in-docker/src/run-test.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -euxo pipefail
 
-# start docker daemon
-nohup /usr/bin/dockerd --storage-driver=devicemapper --data-root /tmp/docker --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375&
+source "$HOME/.profile"
+cat $HOME/.profile
+nvm use default
 
+# start docker daemon
+nohup dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375&
+
+DOCKER_HOST=tcp://127.0.0.1:2375
 # wait until daemon is ready :)
 timeout 15 sh -c "until docker info; do echo .; sleep 1; done"
 
 cd /usr/app
 
 yarn install --no-lockfile
+
+docker-compose -f docker-compose.yml build
 yarn test:docker-in-docker
