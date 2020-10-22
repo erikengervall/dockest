51a66d81437a7f75747431c71dc6239dec208fc6
Erik Engervall <erik.engervall@gmail.com>
2020-10-22 18:34:58 +0200

    Extend website's eslint with react plugin

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -16,5 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
   },
   plugins: ['react'],
-  rules: {},
+  rules: {
+    'react/prop-types': 'off',
+  },
 }
