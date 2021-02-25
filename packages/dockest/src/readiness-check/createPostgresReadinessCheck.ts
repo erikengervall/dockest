1c42dc75c616341b525bc46dc1b122171270110b
n1ru4l <laurinquast@googlemail.com>
2021-02-25 14:32:50 +0100

    feat: add support for POSTGRES_PASSWORD environment variable

--- packages/dockest/src/readiness-check/createPostgresReadinessCheck.ts
+++ packages/dockest/src/readiness-check/createPostgresReadinessCheck.ts
@@ -6,6 +6,7 @@ import { execaWrapper } from '../utils/execaWrapper'
 type Config = {
   POSTGRES_DB: string
   POSTGRES_USER: string
+  POSTGRES_PASSWORD?: string
 }
 
 type MaybePromise<T> = T | Promise<T>
@@ -14,13 +15,22 @@ type ConfigMapper = Config | ((dockerComposeFileService: Runner) => MaybePromise
 
 const postgresReadinessCheck = (configMapper: ConfigMapper): ReadinessCheck => async args => {
   const config = await (typeof configMapper === 'function' ? configMapper(args.runner) : configMapper)
+
+  if (!config.POSTGRES_DB) {
+    throw new Error("Value 'POSTGRES_DB' was not provided.")
+  }
+  if (!config.POSTGRES_USER) {
+    throw new Error("Value 'POSTGRES_USER' was not provided.")
+  }
+
+  const passwordEnvironmentVariable = config.POSTGRES_PASSWORD ? `PGPASSWORD='${config.POSTGRES_PASSWORD}'` : ''
   // Ref: http://postgresguide.com/utilities/psql.html
   const command = `docker exec ${args.runner.containerId} bash -c " \
-                        psql \
-                        -h localhost \
-                        -d ${config.POSTGRES_DB} \
-                        -U ${config.POSTGRES_USER} \
-                        -c 'select 1'"`
+                      ${passwordEnvironmentVariable} psql \
+                      -h localhost \
+                      -d ${config.POSTGRES_DB} \
+                      -U ${config.POSTGRES_USER} \
+                      -c 'select 1'"`
 
   await execaWrapper(command, { runner: args.runner })
 }
