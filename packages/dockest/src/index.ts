8402fe42c1cdfe2924032a9df312887f3f3fa4bc
n1ru4l <laurinquast@googlemail.com>
2021-02-26 08:27:51 +0100

    feat: export DockestService type.

--- packages/dockest/src/index.ts
+++ packages/dockest/src/index.ts
@@ -13,7 +13,7 @@ export { execaWrapper as execa } from './utils/execaWrapper'
 export { LOG_LEVEL as logLevel } from './constants'
 export { sleep } from './utils/sleep'
 export { sleepWithLog } from './utils/sleepWithLog'
-// export { createContainerIsHealthyHealthcheck } from './utils/createContainerIsHealthyHealthcheck'
+export { DockestService } from './@types'
 
 export class Dockest {
   private config: DockestConfig
