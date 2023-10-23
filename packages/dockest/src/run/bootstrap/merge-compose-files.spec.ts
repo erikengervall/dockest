997ef97a46042620e45aba977813046e8ab4b9f5
Erik Engervall <erik.engervall@gmail.com>
2023-10-23 14:49:37 +0900

    update tests

--- packages/dockest/src/run/bootstrap/merge-compose-files.spec.ts
+++ packages/dockest/src/run/bootstrap/merge-compose-files.spec.ts
@@ -1,3 +1,4 @@
+import { safeLoad } from 'js-yaml';
 import { mergeComposeFiles } from './merge-compose-files';
 
 const nodeProcess: any = { cwd: () => __dirname };
@@ -49,21 +50,29 @@ describe('mergeComposeFiles', () => {
     it('should work for single compose file', async () => {
       const { mergedComposeFiles } = await mergeComposeFiles('merge-compose-files.spec.yml', nodeProcess);
 
-      expect(mergedComposeFiles).toMatchInlineSnapshot(`
-        "name: bootstrap
-        services:
-          redis:
-            image: redis:5.0.3-alpine
-            networks:
-              default: null
-            ports:
-            - target: 6379
-              published: "6379"
-              protocol: tcp
-        networks:
-          default:
-            name: bootstrap_default"
-      `);
+      expect(safeLoad(mergedComposeFiles)).toMatchObject({
+        name: 'bootstrap',
+        networks: {
+          default: {
+            name: 'bootstrap_default',
+          },
+        },
+        services: {
+          redis: {
+            image: 'redis:5.0.3-alpine',
+            networks: {
+              default: null,
+            },
+            ports: [
+              {
+                protocol: 'tcp',
+                published: '6379',
+                target: 6379,
+              },
+            ],
+          },
+        },
+      });
     });
 
     it('should work for multiple compose files', async () => {
@@ -71,41 +80,59 @@ describe('mergeComposeFiles', () => {
         ['merge-compose-files.spec.yml', 'merge-compose-files-2.spec.yml'],
         nodeProcess,
       );
-      expect(mergedComposeFiles).toMatchInlineSnapshot(`
-        "name: bootstrap
-        services:
-          postgres:
-            environment:
-              POSTGRES_DB: nobueno
-              POSTGRES_PASSWORD: is
-              POSTGRES_USER: ramda
-            image: postgres:9.6-alpine
-            networks:
-              default: null
-            ports:
-            - target: 5432
-              published: "5433"
-              protocol: tcp
-          redis:
-            image: redis:5.0.3-alpine
-            networks:
-              default: null
-            ports:
-            - target: 6379
-              published: "6379"
-              protocol: tcp
-        networks:
-          default:
-            name: bootstrap_default"
-      `);
+
+      expect(safeLoad(mergedComposeFiles)).toMatchObject({
+        name: 'bootstrap',
+        networks: {
+          default: {
+            name: 'bootstrap_default',
+          },
+        },
+        services: {
+          postgres: {
+            environment: {
+              POSTGRES_DB: 'nobueno',
+              POSTGRES_PASSWORD: 'is',
+              POSTGRES_USER: 'ramda',
+            },
+            image: 'postgres:9.6-alpine',
+            networks: {
+              default: null,
+            },
+            ports: [
+              {
+                protocol: 'tcp',
+                published: '5433',
+                target: 5432,
+              },
+            ],
+          },
+          redis: {
+            image: 'redis:5.0.3-alpine',
+            networks: {
+              default: null,
+            },
+            ports: [
+              {
+                protocol: 'tcp',
+                published: '6379',
+                target: 6379,
+              },
+            ],
+          },
+        },
+      });
     });
   });
 
   describe('sad', () => {
     it('should throw if invalid name of compose file', async () => {
-      const promise = mergeComposeFiles('this-file-does-not-exist.yml', nodeProcess);
-
-      await expect(promise).rejects.toThrow('Invalid Compose file(s)');
+      try {
+        await mergeComposeFiles('this-file-does-not-exist.yml', nodeProcess);
+        expect(true).toBe('Should throw.');
+      } catch (error) {
+        expect(error).toMatchInlineSnapshot(`[DockestError: Invalid Compose file(s)]`);
+      }
     });
   });
 });
